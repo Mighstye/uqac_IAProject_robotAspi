b1775e6ee671adbb24af3c038caf5dd0ceed8060
Begin Florian <misterluckyyt@gmail.com>
2022-01-29 23:02:51 -0500

    debut environnement + robot

---
+++ AIProject/enum/cardinals.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Cardinals(Enum):
+    NORTH = 'north'
+    SOUTH = 'south'
+    WEST = 'west'
+    EAST = 'east'
---
+++ AIProject/enum/tilestypes.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Tilestypes(Enum):
+    POUSSIERE = 'poussiere'
+    BIJOUX = 'bijoux'
+    DOUBLE = 'poussiere+bijoux'
+    RIEN = 'rien'
